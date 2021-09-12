90f63b78df158c422e8aa83ea9c8ebea7d318542
FaizaNadeem90 <90497269+FaizaNadeem90@users.noreply.github.com>
2021-09-12 13:33:20 +0500

    hi

--- New folder/Assets/Scripts/CameraFollow.cs
+++ New folder/Assets/Scripts/CameraFollow.cs
@@ -5,21 +5,16 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    Vector3 offset;
+    private Vector3 _target = Vector3.zero;
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        offset = transform.position - player.position;
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = player.position + offset;
-        targetPos.x = 0;
-        transform.position = targetPos;
+        _target = new Vector3 (player.position.x, player.position.y + 2, player.position.z - 3);
+        transform.position = Vector3.Lerp(transform.position, _target, 1f);
     }
 }
