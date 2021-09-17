7d671d00ea14cedc1a5505f7d84e78c76203f92c
manahilj <manahiljawaid@gmail.com>
2021-09-18 01:09:38 +0500

    humans

--- New folder/Assets/Scripts/humancontroller.cs
+++ New folder/Assets/Scripts/humancontroller.cs
@@ -6,28 +6,34 @@ public class humancontroller : MonoBehaviour
 {
 
     private Transform _player;
-    public int speed=3;
+    private Animator _animator;
+    public int speed=8;
     public int distance=10;
     void Start()
     {
         _player=GameObject.Find("Player").transform;
+        _animator = GetComponent<Animator>();
+        
 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         if(!GameObject.Find("Player").GetComponent<playercontroller> ().isDead){
             if(Vector3.Distance(transform.position,_player.position)<= distance){
                 transform.position= new Vector3 (transform.position.x,transform.position.y,transform.position.z - speed*Time.deltaTime);
             }
         }
     }
+   
 
     void OnTriggerEnter(){
       // macheteAudio.Play();
-       Destroy(this.gameObject);
-
-       Sliderr.slidervalue++;
+      _animator.SetTrigger("Standing React Death Backward");
+       //Destroy(this.gameObject);
+      
+       Sliderr.slidervalue+=2;
    }
 }
