95e20aa6d097c904b36b5f60370481dfd8354ff7
manahilj <manahiljawaid@gmail.com>
2021-09-22 04:49:43 +0500

    4.49

---
+++ New folder/Assets/Scripts/maincan.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maincan : MonoBehaviour
+{
+    public GameObject mainCanvas;
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+     Application.LoadLevel(Application.loadLevel);
+     mainCanvas.SetActive(false);
+     Time.timeScale = 1;   
+    }
+    void Start(){
+        mainCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    public void Restart()
+    {
+        
+    }
+}
