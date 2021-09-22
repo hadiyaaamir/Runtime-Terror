ec8aa77d1c267ff3b2fa3768af77fcf8b0212d4d
manahilj <manahiljawaid@gmail.com>
2021-09-22 14:47:08 +0500

    last night

--- New folder/Assets/Scripts/MainCanvas.cs
+++ New folder/Assets/Scripts/MainCanvas.cs
@@ -7,11 +7,11 @@ public class MainCanvas : MonoBehaviour
 {
     public GameObject mainCanvas;
     // Start is called before the first frame update
-    void Start()
-    {
-         mainCanvas.SetActive(true);
-        Time.timeScale = 0;
-    }
+    // void Start()
+    // {
+    //      mainCanvas.SetActive(true);
+    //     Time.timeScale = 0;
+    // }
 
     // Update is called once per frame
     public void StartGame()
--- New folder/Assets/Scripts/maincan.cs
+++ New folder/Assets/Scripts/maincan.cs
@@ -8,7 +8,7 @@ public class maincan : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-     Application.LoadLevel(Application.loadLevel);
+     Application.LoadLevel(Application.loadedLevel);
      mainCanvas.SetActive(false);
      Time.timeScale = 1;   
     }
